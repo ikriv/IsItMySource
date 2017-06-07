3c0b4581a8343bfd966407d961be73fac61d1e1b
Ivan Krivyakov <ivan@ikriv.com>
2017-06-06 22:50:13 -0400

    Moved assembly version information to a common file

--- src/IsItMySource/Properties/AssemblyInfo.cs
+++ src/IsItMySource/Properties/AssemblyInfo.cs
@@ -21,18 +21,4 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9b20549f-dc1f-4b04-a1d5-2f0d843f279f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
 [assembly: InternalsVisibleTo("IsItMySource.Tests")]
